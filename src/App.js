7d72f77afd4cab7d3b990f11faf36a4bc9e154c7
user-faiz <email2faizm@gmail.com>
2023-09-19 22:51:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import './App.css';
-import Header from './components/navigation/Header';
-
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Rockets from './components/Rockets';
+import Missions from './components/Missions';
+import MyProfile from './components/MyProfile';
+import Navigation from './components/Navigation'; // Import the Navigation component
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      Space Travellers made by Faiz and Shaun
-    </div>
+    <Router>
+      <Navigation /> {/* Render the Navigation component */}
+      <Switch>
+        <Route path="/rockets" component={Rockets} />
+        <Route path="/missions" component={Missions} />
+        <Route path="/myprofile" component={MyProfile} />
+      </Switch>
+    </Router>
   );
 }
 
