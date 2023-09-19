1648a6f25318375a6a96bd9cf0a1302ec7483256
user-faiz <email2faizm@gmail.com>
2023-09-19 23:54:47 +0530

    update store.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Rockets from './components/Rockets';
-import Missions from './components/Redux/missions';
-import MyProfile from './components/MyProfile';
-import Navigation from './components/Navigation'; // Import the Navigation component
+import Profile from './components/MyProfile';
+import Missions from './components/Navigation'; // Import the Navigation component
 
 function App() {
   return (
     <Router>
-      <Navigation /> {/* Render the Navigation component */}
-      <Switch>
-        <Route path="/rockets" component={Rockets} />
-        <Route path="/missions" component={Missions} />
-        <Route path="/myprofile" component={MyProfile} />
-      </Switch>
+      <NavBar />
+      <Routes>
+        <Route path="/Missions" element={<Missions />} />
+        <Route path="/MyProfile" element={<Profile />} />
+        <Route path="/Rockets" element={<Rockets />} />
+
+      </Routes>
     </Router>
   );
 }
